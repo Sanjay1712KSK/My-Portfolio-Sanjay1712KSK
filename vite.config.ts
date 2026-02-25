9559b5695111696da93fc66e0e9d5f4ac1a1df46
Sanjay Kumar S <sanjayksk1712@gmail.com>
2026-02-26 00:13:28 +0530

    github stats page major update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",   // Root domain deployment
+  base: "Sanjay1712KSK.github.io",   // Root domain deployment
   plugins: [react()],
 })
